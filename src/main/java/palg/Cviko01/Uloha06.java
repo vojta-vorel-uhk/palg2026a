d523a3a4d5d352b43aa0d35aaa868e0b62d7966d
Vojta Vorel <vorelvo2@uhk.cz>
2026-09-21 12:27:27 +0200

    Během cvika 1

---
+++ src/main/java/palg/Cviko01/Uloha06.java
@@ -0,0 +1,24 @@
+package palg.Cviko01;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Uloha06 {
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("Zadej město:");
+            Scanner s = new Scanner(System.in);
+            String city = s.nextLine();
+            if (isOk(city))
+                System.out.println("OK");
+            else
+                System.out.println("NOT_OK");
+        }
+    }
+
+    public static boolean isOk(String input)
+    {
+        var stream = Arrays.stream(input.split(" "));
+        return stream.filter(p->p.length()<=2).count() > 1;
+    }
+}
